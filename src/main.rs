dbce9f892ea66dfde2c6d793797991a32fa39646
Edoardo Morandi <morandidodo@gmail.com>
2019-05-30 20:39:32 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct ArgsOpt {
+    #[structopt(long = "exclude-prefix")]
+    exclude_prefix: Option<PathBuf>,
+
+    #[structopt(long = "prefix")]
+    prefix: Option<PathBuf>,
+
+    #[structopt(long = "boot")]
+    boot: bool,
+
+    #[structopt(long = "create")]
+    create: bool,
+
+    #[structopt(long = "remove")]
+    remove: bool,
+
+    #[structopt(long = "clean")]
+    clean: bool,
+
+    #[structopt(long = "verbose", short = "v")]
+    verbose: bool,
+
+    #[structopt(long = "dry-run")]
+    dry_run: bool,
+}
+
+fn main() {
+    let args_opt = ArgsOpt::from_args();
+
+    dbg!(args_opt);
+}
