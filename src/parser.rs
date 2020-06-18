87fddca50f2b4c9860f0c764bef70675bae73795
Alessandro Baldo <baldoalessandro@users.noreply.github.com>
2020-06-19 00:34:40 +0200

    Wire item_type parser into line parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,9 @@
 use std::convert::TryFrom;
 
-use nom::IResult;
+use nom::character::complete::{char, one_of, space1};
 use nom::combinator::{map_res, opt};
-use nom::character::complete::{char, one_of};
 use nom::sequence::tuple;
-
+use nom::IResult;
 
 // NOTE: taken from systemd source code
 // https://github.com/systemd/systemd/blob/3a712fda86ea7d7dc1082b1332f9e94d19c0739a/src/tmpfiles/tmpfiles.c#L73
@@ -27,7 +26,7 @@ pub enum ItemTypes {
     _ADJUST_MODE,
     RELABEL_PATH,
     _RECURSIVE_RELABEL_PATH,
-    _CREATE_FILE,
+    CREATE_FILE,
     _TRUNCATE_FILE,
     // TODO: adds missing cases
 }
@@ -51,49 +50,55 @@ impl TryFrom<char> for ItemTypes {
 
     fn try_from(type_char: char) -> Result<Self, Self::Error> {
         match type_char {
+            'f' => Ok(ItemTypes::CREATE_FILE),
             'z' => Ok(ItemTypes::RELABEL_PATH),
-            _ => Err("Not implemented")
+            _ => Err("Not implemented"),
         }
     }
 }
 
-
-
 fn item_type_char(input: &[u8]) -> IResult<&[u8], ItemTypes> {
-    map_res(
-        one_of(VALID_ITEM_TYPES),
-        ItemTypes::try_from
-    )(input)
+    map_res(one_of(VALID_ITEM_TYPES), ItemTypes::try_from)(input)
 }
 
 fn item_type(input: &[u8]) -> IResult<&[u8], (ItemTypes, bool, bool, bool)> {
-    
-    let (input, (i_type, boot_only, append_or_force, allow_failure)) = tuple(
-        (item_type_char, opt(char('!')), opt(char('+')), opt(char('-')))
-    )(input)?;
-    Ok((input, (
-        i_type,
-        boot_only.is_some(),
-        append_or_force.is_some(),
-        allow_failure.is_some()
-    )))
+    let (input, (i_type, boot_only, append_or_force, allow_failure)) = tuple((
+        item_type_char,
+        opt(char('!')),
+        opt(char('+')),
+        opt(char('-')),
+    ))(input)?;
+    let (input, _ ) = space1(input)?;
+
+    Ok((
+        input,
+        (
+            i_type,
+            boot_only.is_some(),
+            append_or_force.is_some(),
+            allow_failure.is_some(),
+        ),
+    ))
 }
 
+fn parse_line(input: &[u8]) -> IResult<&[u8], Action> {
+    let (input, (action_type, boot_only, append_or_force, allow_failure)) = item_type(input)?;
 
-fn parse_line(line: &[u8]) -> IResult<&[u8], Action> {
-
-    Ok((line, Action {
-        action_type: ItemTypes::RELABEL_PATH,
-        path: "/tmp/z/f",
-        mode: "0755",
-        user: "daemon",
-        group: "daemon",
-        age: "-",
-        argument: "-",
-        boot_only: false,
-        append_or_force: false,
-        allow_failure: false,
-    }))
+    Ok((
+        input,
+        Action {
+            action_type,
+            path: "/tmp/z/f",
+            mode: "0755",
+            user: "daemon",
+            group: "daemon",
+            age: "-",
+            argument: "-",
+            boot_only,
+            append_or_force,
+            allow_failure,
+        },
+    ))
 }
 
 #[cfg(test)]
@@ -102,9 +107,36 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_parse_line() {
-        let res = parse_line(b"z     /tmp/z/f1    0755 daemon daemon - -");
+    fn test_parse_item_type() {
+        assert_eq!(
+            (ItemTypes::RELABEL_PATH, false, false, false),
+            item_type(b"z ").unwrap().1
+        );
 
+        assert_eq!(
+            (ItemTypes::RELABEL_PATH, true, false, false),
+            item_type(b"z! ").unwrap().1
+        );
+
+        assert_eq!(
+            (ItemTypes::RELABEL_PATH, false, true, false),
+            item_type(b"z+ ").unwrap().1
+        );
+
+        assert_eq!(
+            (ItemTypes::RELABEL_PATH, false, false, true),
+            item_type(b"z- ").unwrap().1
+        );
+    }
+
+    #[test]
+    fn test_parse_item_type_error() {
+        assert!(item_type(b"y ").is_err());
+        assert!(item_type(b"foobar").is_err());
+    }
+
+    #[test]
+    fn test_parse_line() {
         assert_eq!(
             Action {
                 action_type: ItemTypes::RELABEL_PATH,
@@ -118,42 +150,27 @@ mod test {
                 append_or_force: false,
                 allow_failure: false,
             },
-            res.unwrap().1
+            parse_line(b"z     /tmp/z/f1    0755 daemon daemon - -")
+                .unwrap()
+                .1
+        );
+
+        assert_eq!(
+            Action {
+                action_type: ItemTypes::CREATE_FILE,
+                path: "/tmp/z/f",
+                mode: "0755",
+                user: "daemon",
+                group: "daemon",
+                age: "-",
+                argument: "-",
+                boot_only: false,
+                append_or_force: false,
+                allow_failure: false,
+            },
+            parse_line(b"f     /tmp/z/f1    0755 daemon daemon - -")
+                .unwrap()
+                .1
         );
     }
-
-    #[test]
-    fn test_parse_item_type() {
-        assert_eq!(
-            (ItemTypes::RELABEL_PATH, false, false, false),
-            item_type(b"z").unwrap().1
-        );
-
-        assert_eq!(
-            (ItemTypes::RELABEL_PATH, true, false, false),
-            item_type(b"z!").unwrap().1
-        );
-
-        assert_eq!(
-            (ItemTypes::RELABEL_PATH, false, true, false),
-            item_type(b"z+").unwrap().1
-        );
-
-        assert_eq!(
-            (ItemTypes::RELABEL_PATH, false, false, true),
-            item_type(b"z-").unwrap().1
-        );
-    }
-
-    #[test]
-    fn test_parse_item_type_error() {
-        assert!(
-            item_type(b"y").is_err()
-        );
-
-        assert!(
-            item_type(b"foobar").is_err()
-        );
-    }
-
 }
